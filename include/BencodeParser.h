bb965901a33bb2674d0a9b1380262258b318fcf8
propruhh <propruhh@gmail.com>
2021-05-28 18:56:51 -0500

    pro documentation

--- include/BencodeParser.h
+++ include/BencodeParser.h
@@ -3,6 +3,13 @@
 
 @interface BencodeParser : NSObject {
     char state;
+    /* 
+    'i' = integer
+    's' = string
+    'l' = list
+    'd' = dictionary
+    'e' = nothing
+    */
 }
 
 + (int)torrentLength:(NSString*)torrentURL;
